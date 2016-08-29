fb0723017b6717787fd793e9f7f99dbbe87a89c0
Noel Ferreira <noel.ferreira@gmail.com>
2016-08-29 08:02:30 -0400

    #1 First Facebook login attempt

--- BebEvo/ViewController.swift
+++ BebEvo/ViewController.swift
@@ -2,24 +2,61 @@
 //  ViewController.swift
 //  BebEvo
 //
-//  Created by Noel Ferreira on 8/29/16.
+//  Created by Noel Ferreira on 8/28/16.
 //  Copyright © 2016 Noel Ferreira. All rights reserved.
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if FBSDKAccessToken.currentAccessToken() == nil {
+            print("Not logged in..")
+        }
+        else
+        {
+            print("Logged in..")
+        }
+        
+        let loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.center = self.view.center
+        
+        loginButton.delegate = self
+        
+        self.view.addSubview(loginButton)
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func loginButton(logginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!){
+        if error == nil {
+            print("Login complete.")
+            self.performSegueWithIdentifier("showNew", sender: self)
+        }
+        else
+        {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User logged out...")
+    }
+    
+    
+    
 }
 
